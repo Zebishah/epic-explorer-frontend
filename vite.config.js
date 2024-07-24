a19e0ff1e7910d966062aebfaca3efa11aeb1c7a
Zebishah <zebihaider123@gmail.com>
2024-07-24 20:43:55 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "public", // Output directory
-    index: "./public/index.html", // Index file name
+    outDir: "public",
+    entry: "./public/index.html", // Specify the entry point
   },
 });
